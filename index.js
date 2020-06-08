5c08e4582a379b69e9c955df08983c56b8264f26
olucheye <seyefree2rhyme@yahoo.com>
2020-06-08 13:08:05 +0100

    updated initial commit

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ const express = require('express');
 const multer = require('multer');
 const app = express();
 
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const get = require();
+const post = require();
+const update = require();
+const remove = require();
+
+app.use('api/v1/', get);
+app.use('api/v1/upload', post);
+app.use('api/v1/:id', update);
+app.use('api/v1/:id', remove);
 
 const port = (process.env.PORT || 3000);
 app.listen(port, ()=> `Server now running on ${port}`)
