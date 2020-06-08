7f78886c50dfc6872b843ecf4853d6e99276a533
olucheye <seyefree2rhyme@yahoo.com>
2020-06-08 16:40:21 +0100

    minor changes to model

--- models/file.model.js
+++ models/file.model.js
@@ -3,15 +3,10 @@ const shortid = require('shortid');
 
 //define database schema & model for posts
 const fileSchema = new mongoose.Schema({
-    _id: {
-        type: String,
-        default: shortid.generate
-    },
+    _id: {type: String, default: shortid.generate},
     title: { type: String},
-    time:{
-        type: Date,
-        default: Date.now
-    }
+    fileURL: { type: String},
+    time:{ type: Date, default: Date.now }
   });
   
   const File = mongoose.model("Upload", fileSchema);
